372a50d10c2485c9daa547c14b35dc1115a74d9a
novel36 <novelwolde36@gmail.com>
2022-04-04 16:37:38 +0300

    d

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'todo.apps.TodoConfig'
 ]
 
@@ -120,5 +120,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-# STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles')
